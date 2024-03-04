d3f2e5d06710722abaa39f476b5142e3ae40388a
vincebout <vboulant@hotmail.fr>
2024-03-04 18:44:01 +0100

    feat: add PostgreSQL database

--- app/main.py
+++ app/main.py
@@ -1,8 +1,27 @@
 """ Main file of the API """
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from .routers import users
+from .database.database import init_tables, postgreSQL_pool
+from .internal.log_config import logger
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app):
+    """ Startup and close methods """
+    # Init tables if not created
+    logger.info("Connecting to database")
+    conn = postgreSQL_pool.getconn()
+    init_tables(conn)
+    postgreSQL_pool.putconn(conn)
+    logger.info("Connected to database")
+    yield
+    # Close connection at shutdown
+    if postgreSQL_pool:
+        logger.info("Closing connection to database")
+        postgreSQL_pool.closeall()
+        logger.info("Connection to database closed")
+
+app = FastAPI(lifespan=lifespan)
 app.include_router(users.router)
 
 
